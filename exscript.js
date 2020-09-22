58f98b69ad0be1e5976da0750e341d2f0f14e9ea
Lucas Cavalcante de Rezende <luucasskywalker@gmail.com>
2020-09-22 20:22:16 -0300

    Uso do setInterval
    referência para um futuro uso, let seconds dentro do bloco . Não consiguia criar uma variável dentro do bloco e usar ela externa, tive que criar dentro .

--- exscript.js
+++ exscript.js
@@ -1,4 +1,4 @@
-function carregar () {
+function load () {
     let msg = document.querySelector('div#mensagem');
     let img = document.getElementById('img');
     let photo = document.getElementById('foto');
@@ -6,51 +6,52 @@ function carregar () {
     let dateToday = document.getElementById('datadehoje');
     let dateNow = new Date();
     let date = dateNow.getDay();
-    
-    
-    
-    setInterval (()=> {
-        
-        let timetoday = new Date();
-        let time = timetoday.getHours();
-    
-        let minutesToday = new Date();
-        let minutes = minutesToday.getMinutes();
 
-        let secondsToday = new Date();
-        let seconds = secondsToday.getSeconds();
-          
-        if (time > 7 && time <= 12){
-            img.src = 'dia.png'
-            msg.style.color= 'wheat'
-            photo.style.background = 'wheat'
-            document.body.style.background = 'wheat'
-            msg.innerHTML = `<strong>BOM DIA</strong>, ${time}:${minutes}:${seconds} hrs!`
-       
-        } else if (time >=13 && time <= 18 ){
-            img.src = 'tarde.png'
-            msg.style.color= 'orange'
-            photo.style.background = 'orange'
-            document.body.style.background = 'orange'
-            msg.innerHTML = `<strong>BOA TARDE</strong>, ${time}:${minutes}:${seconds} hrs!`
-       
-        } else if (time > 18 && time < 23){
-            img.src = 'noite.png'
-            msg.style.color= 'cornflowerblue'
-            photo.style.background = 'cornflowerblue'
-            document.body.style.background = 'cornflowerblue'
-            msg.innerHTML = `<strong>BOA NOITE</strong>, ${time}:${minutes}:${seconds} hrs!`
-       
-        } else {
-            img.src = 'madrugada.png'
-            msg.style.color = 'cadetblue'
-            photo.style.background = 'cadetblue'
-            document.body.style.background= 'cadetblue'
-            msg.innerHTML = `<strong>BOA MADRUGADA</strong>, ${time}:${minutes}:${seconds} hrs!`
-       
-        }
+    let timeToday = new Date();
+    let time = timeToday.getHours();
+
+    let minutesToday = new Date();
+    let minutes = minutesToday.getMinutes();
+
+  
+
+    
+
+    setInterval(()=>{
+      let secondsToday = new Date();
+      let seconds = secondsToday.getSeconds();
+     
+      msg.innerHTML = `<strong>BOA NOITE</strong>, ${time}:${minutes}:${seconds} hrs!`
 
     },1000)
+     
+     
+    if (time > 7 && time <= 12){
+      img.src = 'dia.png'
+      msg.style.color= 'wheat'
+      photo.style.background = 'wheat'
+      document.body.style.background = 'wheat'
+ 
+  } else if (time >=13 && time <= 18 ){
+      img.src = 'tarde.png'
+      msg.style.color= 'orange'
+      photo.style.background = 'orange'
+      document.body.style.background = 'orange'
+    
+  } else if (time > 18 && time < 23){
+      img.src = 'noite.png'
+      msg.style.color= 'cornflowerblue'
+      photo.style.background = 'cornflowerblue'
+      document.body.style.background = 'cornflowerblue'
+     
+  } else {
+      img.src = 'madrugada.png'
+      msg.style.color = 'cadetblue'
+      photo.style.background = 'cadetblue'
+      document.body.style.background= 'cadetblue'
+ 
+  }
+    
     
        switch (date){
             case 0:
@@ -77,6 +78,6 @@ function carregar () {
             break;        
        }   
     
-    }
-    
+
+      }  
     
