35323989956f7b6c09a5c12ea12d1133bbdcd271
Lucas Cavalcante de Rezende <luucasskywalker@gmail.com>
2020-09-25 23:24:23 -0300

    end ..

--- exscript.js
+++ exscript.js
@@ -26,67 +26,68 @@ function load () {
       let seconds = secondsToday.getSeconds();
 
       if (time > 7 && time <= 12){
-        img.src = 'dia.png'
-        msg.style.color= 'wheat'
-        photo.style.background = 'wheat'
-        document.body.style.background = 'wheat'
-        `<strong>BOM DIA</strong>, ${time}:${minutes}:${seconds} hrs!`
+        img.src = 'dia.png';
+        msg.style.color= 'wheat';
+        photo.style.background = 'wheat';
+        document.body.style.background = 'wheat';
+        `<strong>BOM DIA</strong>, ${time}:${minutes}:${seconds} hrs!`;
         
    
     } else if (time >=13 && time <= 18 ){
-        img.src = 'tarde.png'
-        msg.style.color= 'orange'
-        photo.style.background = 'orange'
-        document.body.style.background = 'orange'
-        msg.innerHTML = `<strong>BOA TARDE</strong>, ${time}:${minutes}:${seconds} hrs!`
+        img.src = 'tarde.png';
+        msg.style.color= 'orange';
+        photo.style.background = 'orange';
+        document.body.style.background = 'orange';
+        msg.innerHTML = `<strong>BOA TARDE</strong>, ${time}:${minutes}:${seconds} hrs!`;
   
       
     } else if (time > 18 && time < 23){
-        img.src = 'noite.png'
-        msg.style.color= 'cornflowerblue'
-        photo.style.background = 'cornflowerblue'
-        document.body.style.background = 'cornflowerblue'
-        msg.innerHTML = `<strong>BOA NOITE</strong>, ${time}:${minutes}:${seconds} hrs!`
+        img.src = 'noite.png';
+        msg.style.color= 'cornflowerblue';
+        photo.style.background = 'cornflowerblue';
+        document.body.style.background = 'cornflowerblue';
+        msg.innerHTML = `<strong>BOA NOITE</strong>, ${time}:${minutes}:${seconds} hrs!`;
   
        
     } else {
-        img.src = 'madrugada.png'
-        msg.style.color = 'cadetblue'
-        photo.style.background = 'cadetblue'
-        document.body.style.background= 'cadetblue'
-        `<strong>BOA MADRUGADA</strong>, ${time}:${minutes}:${seconds} hrs!`
+        img.src = 'madrugada.png';
+        msg.style.color = 'cadetblue';
+        photo.style.background = 'cadetblue';
+        document.body.style.background= 'cadetblue';
+        `<strong>BOA MADRUGADA</strong>, ${time}:${minutes}:${seconds} hrs!`;
    
     }
 
-    },1000)
+    },1000);
    
     
     // switch vinculado a div que retorna a Data da semana ...
+    
        switch (date){
             case 0:
-              dateToday.innerHTML = ` <strong>Domingo</strong>` 
+              dateToday.innerHTML = ` <strong>Domingo</strong>` ;
             break;
             case 1:
-              dateToday.innerHTML = `<strong>Segunda-feira</strong>` 
+              dateToday.innerHTML = `<strong>Segunda-feira</strong>`; 
             break;      
             case 2:
-              dateToday.innerHTML = '<strong>Terça-feira</strong>'
+              dateToday.innerHTML = '<strong>Terça-feira</strong>';
             break;  
             case 3:
-              dateToday.innerHTML = '<strong>Quarta-feira</strong>'
+              dateToday.innerHTML = '<strong>Quarta-feira</strong>';
             break;
             case 4:
-              dateToday.innerHTML = '<strong>Quinta-feira</strong>' 
+              dateToday.innerHTML = '<strong>Quinta-feira</strong>' ;
             break;
             case 5:
-              dateToday.innerHTML = '<strong>Sexta-feira</strong>'  
+              dateToday.innerHTML = '<strong>Sexta-feira</strong>';  
             break;
             case 6:
-              dateToday.innerHTML = '<strong>Sabado</strong>'  
+              dateToday.innerHTML = '<strong>Sabado</strong>' ; 
             default:
             break;        
-       }   
+       }; 
     
 
-      }  
+      };
     
